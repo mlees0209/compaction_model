62dc1eca0a21d3e84ec15fb3349c7bbab4261709
Matthew Lees <mlees@stanford.edu>
2020-10-28 16:45:25 -0700

    making input data a nice histogram type plot

--- execute_model.py
+++ execute_model.py
@@ -320,10 +320,10 @@ if len(layers_requiring_solving)>= 0:
     layeri=0
     for layer in layers_requiring_solving:
         if layer_types[layer]=='Aquifer':
-            plt.bar(np.array(list(interbeds_distributions[layer].keys()))+layeri*barwidth,list(interbeds_distributions[layer].values()),width=barwidth,label=layer)
+            plt.bar(np.array(list(interbeds_distributions[layer].keys()),color='None',edgecolor=sns.color_palette()[layeri],linewidth=5,alpha=0.6)+layeri*barwidth,list(interbeds_distributions[layer].values()),width=barwidth,label=layer)
             layeri+=1
         if layer_types[layer]=='Aquitard':
-            plt.bar(layer_thicknesses[layer]+layeri*barwidth,1,width=barwidth,label=layer)
+            plt.bar(layer_thicknesses[layer]+layeri*barwidth,1,width=barwidth,label=layer,color='None',edgecolor=sns.color_palette()[layeri],linewidth=5,alpha=0.6)
             layeri+=1
     plt.legend()
     plt.xlabel('Layer thickness (m)')
