885980df871ff96c063563c7521ae8a10c2aea19
Matthew Lees <mlees@stanford.edu>
2020-11-10 15:58:14 -0800

    now works on bletchley and mac

--- postprocessing_script.sh
+++ postprocessing_script.sh
@@ -5,13 +5,27 @@
 runname=$1
 echo "Runname is "$runname
 
+if test -f "/Users/mlees/Documents/RESEARCH/Land_Subsidence/Local_Scale/MODEL/execute_model.py"; 
+then 
+echo "WE ARE WORKING IN MAC MODE"
 echo "Making deformation data comparisons"
 python /Users/mlees/Documents/RESEARCH/Land_Subsidence/Local_Scale/MODEL/compare_with_deformation_data.py $runname
 python /Users/mlees/Documents/RESEARCH/Land_Subsidence/Local_Scale/MODEL/compare_with_deformation_data.py $runname deephead=True
-
 echo "Plotting aquifer partitioning"
 python /Users/mlees/Documents/RESEARCH/Land_Subsidence/Local_Scale/MODEL/plot_partitioning.py $runname
-
-
 echo "Plotting interbed partitioning"
 python /Users/mlees/Documents/RESEARCH/Land_Subsidence/Local_Scale/MODEL/plot_partitioning_interbeds.py $runname
+fi
+
+if test -f "/home/mlees/Land_Subsidence/Local_Scale/compaction_model/execute_model.py";
+then 
+echo "WE ARE WORKING IN LINIUX MODE"
+echo "Making deformation data comparisons"
+python /home/mlees/Land_Subsidence/Local_Scale/compaction_model/compare_with_deformation_data.py $runname
+python /home/mlees/Land_Subsidence/Local_Scale/compaction_model/compare_with_deformation_data.py $runname deephead=True
+echo "Plotting aquifer partitioning"
+python /home/mlees/Land_Subsidence/Local_Scale/compaction_model/plot_partitioning.py $runname
+echo "Plotting interbed partitioning"
+python /home/mlees/Land_Subsidence/Local_Scale/compaction_model/plot_partitioning_interbeds.py $runname
+fi
+
