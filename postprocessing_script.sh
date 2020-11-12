1b2238638b41713eae5d2bd93ccc17aae83a539c
Matthew <mlees0209@gmail.com>
2020-11-12 11:00:07 -0800

    adding knightblade support

--- postprocessing_script.sh
+++ postprocessing_script.sh
@@ -29,3 +29,16 @@ echo "Plotting interbed partitioning"
 python /home/mlees/Land_Subsidence/Local_Scale/compaction_model/plot_partitioning_interbeds.py $runname
 fi
 
+if test -f "/home/mlees/compaction_model/execute_model.py";
+then
+echo "WE ARE WORKING IN Knightblade MODE"
+echo "Making deformation data comparisons"
+python /home/mlees/compaction_model/compare_with_deformation_data.py $runname
+python /home/mlees/compaction_model/compare_with_deformation_data.py $runname deephead=True
+echo "Plotting aquifer partitioning"
+python /home/mlees/compaction_model/plot_partitioning.py $runname
+echo "Plotting interbed partitioning"
+python /home/mlees/compaction_model/plot_partitioning_interbeds.py $runname
+fi
+
+
