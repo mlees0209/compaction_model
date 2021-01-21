b508c3318a7b8be3dca2e27145da5283f0cda005
Matthew Lees <mlees@stanford.edu>
2021-01-21 11:30:32 -0800

    variuoos updates

--- compare_with_deformation_data.py
+++ compare_with_deformation_data.py
@@ -77,6 +77,7 @@ if deephead:
     H= pd.read_csv('input_data/input_time_series_Lower_Aquifer.csv',header=None)
 
 Data = pd.read_csv('Total_Deformation_Out.csv',parse_dates=[0])
+
 WellHlat = 36.32750
 WellHlon = -119.58056
 
@@ -97,8 +98,8 @@ Poland_75_dates = date2num([date(1954,1,1),date(1957,1,1),date(1958,7,1),date(19
 Poland_75_data = -30.48*  np.array([0,0.83,1.34,3.25,4.92,6.31])
 
 Highway_198_dates = date2num([date(1972,6,1),date(2004,1,1)])
-Highway_198_data = 100*np.array([0,-0.84])
-Highway_198_data_uncertainty = np.array([0,Poland_75_data[4]-Poland_75_data[2]])
+Highway_198_data = 100*np.array([0,-1.7])
+#Highway_198_data_uncertainty = np.array([0,Poland_75_data[4]-Poland_75_data[2]])
 
 # Jacobus_data = pd.read_excel('/Users/mlees/Documents/RESEARCH/ground_deformation/GPS/From_Matt_Jacobus/Jacobus_Data_MASTER.xlsx')
 # Jacobus_data['YRMO']= pd.to_datetime(Jacobus_data['YRMO'],format='%Y%b')
@@ -115,6 +116,19 @@ if knightblade:
 
 Envisat = import_InSAR_csv(envisat_file)
 Envisat_dates,Envisat_data = extract_series_from_latlon(36.32750,-119.58056,Envisat)
+print('Finding envisat incidence angle.')
+envisat_incidence = Envisat['incidence_angle'].values[np.argmax([np.sum(Envisat.values[i,-24:] == Envisat_data) for i in range(len(Envisat))])]
+print('\tIncidence angle is %.2f deg.' % envisat_incidence)
+print()
+
+KSB0486_lat=36.2811
+KSB0486_lon = -119.6367
+Envisat_dates_KSB0486,Envisat_data_KSB0486 = extract_series_from_latlon(KSB0486_lat,KSB0486_lon,Envisat)
+print('Finding envisat incidence angle at KSB0486.')
+envisat_incidence_KSB0486 = Envisat['incidence_angle'].values[np.argmax([np.sum(Envisat.values[i,-24:] == Envisat_data_KSB0486) for i in range(len(Envisat))])]
+print('\tIncidence angle is %.2f deg.' % envisat_incidence_KSB0486)
+print()
+
 
 
 save = True
@@ -128,6 +142,8 @@ fig,ax1 = plt.subplots(figsize=(18,12))
 
 modelled_data_rezeroed = np.array(100 * rezero_series(Data['Total'],np.array([date2num(date) for date in Data['dates']]),'Jun-1980'))
 
+#modelled_data_adjusted_rezeroed = np.array(100 * rezero_series(Data_variablethickness['Total'],np.array([date2num(date) for date in Data_variablethickness['dates']]),'Jun-1980')
+
 Sentinel_rezeroed = np.array(0.1 * rezero_series(InSAR_H,datesinsarH,'Jan-2016'))
 
 
@@ -135,12 +151,11 @@ ax1.plot_date([date2num(date) for date in Data['dates']][0:365*20],100*rezero_se
 
 ax1.plot_date([date2num(date) for date in Data['dates']][365*20:],100*rezero_series(Data['Total'],np.array([date2num(date) for date in Data['dates']]),'Jun-1980')[365*20:],'b--',label='Modelled (believable)')
 
-
 ax1.plot_date(Poland_75_dates,Poland_75_data + modelled_data_rezeroed[Data['dates']=='1966-04-01'] - Poland_75_data[Poland_75_dates==date2num(date(1966,4,1))],'k.--',label='Poland 1975 levelling surveys')
 
 
 ax1.plot_date(Highway_198_dates,Highway_198_data + modelled_data_rezeroed[Data['dates']=='1972-06-01'],'k^')
-ax1.errorbar(Highway_198_dates[1],Highway_198_data[1] +modelled_data_rezeroed[Data['dates']=='1972-06-01'],xerr=None,yerr=0.5*Highway_198_data_uncertainty[1],fmt='k^',label='Highway 198 data',capsize=5)
+#ax1.errorbar(Highway_198_dates[1],Highway_198_data[1] +modelled_data_rezeroed[Data['dates']=='1972-06-01'],xerr=None,yerr=0.5*Highway_198_data_uncertainty[1],fmt='k^',label='Highway 198 data',capsize=5)
 
 
 ax1.arrow(Swanson_1998_quote_dates[1],30,-(Swanson_1998_quote_dates[1]-Swanson_1998_quote_dates[0]),0,shape='full',head_width=15,head_length=300,facecolor='black',color='black',width=3)
@@ -152,7 +167,8 @@ ax1.plot_date(datesinsarH,Sentinel_rezeroed + modelled_data_rezeroed[np.argmin(n
 
 
 rezero_idx_env =2
-ax1.plot_date(Envisat_dates,0.1* (Envisat_data - Envisat_data[rezero_idx_env]) + modelled_data_rezeroed[np.where([date2num(date) for date in Data['dates']]==Envisat_dates[rezero_idx_env])[0][0]],'r-',label='Envisat InSAR subsidence')
+ax1.plot_date(Envisat_dates,0.1/np.cos(np.deg2rad(envisat_incidence)) * (Envisat_data - Envisat_data[rezero_idx_env]) + modelled_data_rezeroed[np.where([date2num(date) for date in Data['dates']]==Envisat_dates[rezero_idx_env])[0][0]],'p-',label='Envisat InSAR subsidence (vert)')
+# ax1.plot_date(Envisat_dates_KSB0486,0.1/np.cos(np.deg2rad(envisat_incidence_KSB0486)) * (Envisat_data_KSB0486 - Envisat_data_KSB0486[rezero_idx_env]) + modelled_data_rezeroed[np.where([date2num(date) for date in Data['dates']]==Envisat_dates_KSB0486[rezero_idx_env])[0][0]],'r-',label='Envisat InSAR subsidence (KSB-486 vert)')
 
 
 # ax1.plot_date(S224P2data['YRMO'],foot_to_cm* ( S224P2data['ELEf'] - S224P2data['ELEf'][S224P2data['YRMO']==dt(2016,2,1)].values) +  modelled_data_rezeroed[Data['dates']==dt(2016,2,1)],label='S224P2 data')
@@ -260,5 +276,47 @@ if save:
         plt.savefig('figures/compare_with_ENVISAT.svg',bbox_inches='tight')
 
 
+
+
+
+#%% Extra plot for "adjusted"
+
+
+print('Making version with scaled lower aquifer prior to 2015')
+Data_variablethickness = Data.copy()
+Data_variablethickness['Lower Aquifer'][date2num(Data_variablethickness['dates']) <= date2num(dt(2015,1,1))] = Data['Lower Aquifer'][date2num(Data_variablethickness['dates']) <= date2num(dt(2015,1,1))] * 0.5
+Data_variablethickness['Lower Aquifer'][date2num(Data_variablethickness['dates']) > date2num(dt(2015,1,1))] = Data_variablethickness['Lower Aquifer'][date2num(Data_variablethickness['dates']) == date2num(dt(2015,1,1))].values + (Data['Lower Aquifer'][date2num(Data_variablethickness['dates']) > date2num(dt(2015,1,1))] - Data['Lower Aquifer'][date2num(Data_variablethickness['dates']) == date2num(dt(2015,1,1))].values)
+Data_variablethickness['Total'] = Data_variablethickness['Upper Aquifer'] + Data_variablethickness['Corcoran Clay'] + Data_variablethickness['Lower Aquifer']
+
+
+modelled_data_rezeroed_adusted = np.array(100 * rezero_series(Data_variablethickness['Total'],np.array([date2num(date) for date in Data_variablethickness['dates']]),'Jun-1980'))
+Sentinel_rezeroed = np.array(0.1 * rezero_series(InSAR_H,datesinsarH,'Jan-2016'))
+
+
+fig,ax1 = plt.subplots(figsize=(18,12))
+
+
+# ax1.plot_date([date2num(date) for date in Data['dates']][365*20:],100*rezero_series(Data['Total'],np.array([date2num(date) for date in Data['dates']]),'Jun-1980')[365*20:],'b--',label='Modelled (believable)')
+
+ax1.plot_date([date2num(date) for date in Data_variablethickness['dates']][0:365*20],100*rezero_series(Data_variablethickness['Total'],np.array([date2num(date) for date in Data_variablethickness['dates']]),'Jun-1980')[0:365*20],'--',color='grey',linewidth=0.5,label='Modelled (spin up?)')
+
+ax1.plot_date([date2num(date) for date in Data_variablethickness['dates']][365*20:],100*rezero_series(Data_variablethickness['Total'],np.array([date2num(date) for date in Data_variablethickness['dates']]),'Jun-1980')[365*20:],'r--',label='Modelled adjusted (believable)')
+
+ax1.plot_date(Highway_198_dates,Highway_198_data + modelled_data_rezeroed_adusted[Data_variablethickness['dates']=='1972-06-01'],'k^',label='Highway 198 data')
+
+ax1.plot_date(Poland_75_dates,Poland_75_data + modelled_data_rezeroed_adusted[Data['dates']=='1966-04-01'] - Poland_75_data[Poland_75_dates==date2num(date(1966,4,1))],'k.--',label='Poland 1975 levelling surveys')
+
+rezero_idx_env =2
+ax1.plot_date(Envisat_dates,0.1/np.cos(np.deg2rad(envisat_incidence)) * (Envisat_data - Envisat_data[rezero_idx_env]) + modelled_data_rezeroed_adusted[np.where([date2num(date) for date in Data['dates']]==Envisat_dates[rezero_idx_env])[0][0]],'p-',label='Envisat InSAR subsidence (vert)')
+ax1.plot_date(Envisat_dates_KSB0486,0.1/np.cos(np.deg2rad(envisat_incidence_KSB0486)) * (Envisat_data_KSB0486 - Envisat_data_KSB0486[rezero_idx_env]) + modelled_data_rezeroed_adusted[np.where([date2num(date) for date in Data['dates']]==Envisat_dates_KSB0486[rezero_idx_env])[0][0]],'o-',label='Envisat InSAR subsidence (KSB-486 vert)')
+
+ax1.plot_date(datesinsarH,Sentinel_rezeroed + modelled_data_rezeroed_adusted[np.argmin(np.abs(date2num(Data['dates'])- date2num(date(2016,1,1))))],'k-',label='Sentinel InSAR subsidence')
+
+plt.legend()
+
+plt.title('%s (with expanding lower aquifer in 2015)' % directory.split('/')[-1])
+
+if save:
+    plt.savefig('figures/compare_with_measurementsFULL_adjusted.png',bbox_inches='tight')
 
 os.chdir(cwd)
